768755fbc853afcbb2eeb57da25e82517479a769
Refzy Agung <111717405+xoy1337@users.noreply.github.com>
2022-11-19 00:09:48 +0700

    Add files via upload

---
+++ aspirasiwarga/aspirasiwarga.sql
@@ -0,0 +1,215 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 17, 2022 at 06:23 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `siwarga`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `isAdmin`) VALUES
+(1, 'admin', 'admin', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aspirasi`
+--
+
+CREATE TABLE `aspirasi` (
+  `id_aspirasi` int(11) NOT NULL,
+  `id_penduduk` int(11) DEFAULT NULL,
+  `judul_aspirasi` varchar(255) DEFAULT NULL,
+  `lokasi` text DEFAULT NULL,
+  `isi_aspirasi` text DEFAULT NULL,
+  `id_kategori` int(11) DEFAULT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status_aspirasi` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `aspirasi`
+--
+
+INSERT INTO `aspirasi` (`id_aspirasi`, `id_penduduk`, `judul_aspirasi`, `lokasi`, `isi_aspirasi`, `id_kategori`, `tanggal`, `status_aspirasi`) VALUES
+(1, 1, 'Maling Di Rt.03', 'Rumah Pak Doni', 'Ada Maling ketangkap pak tolong segera kemari', 2, '2022-11-17 06:39:47', 1),
+(2, 2, 'Saluran Air Tersumbat', 'Warung Mpok Ida', 'dajiowamdownoanoefjnan olvjn envjunvejn voeaklv neaokvubagoevjkan ', 1, '2022-11-17 00:56:55', 0),
+(4, 2, 'Saluran Air Tersumbat', 'Warung Mpok Ida', 'dajiowamdownoanoefjnan olvjn envjunvejn voeaklv neaokvubagoevjkan ', 1, '2022-11-17 01:02:24', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedbacks`
+--
+
+CREATE TABLE `feedbacks` (
+  `id` int(11) NOT NULL,
+  `feedback` text NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp(),
+  `isAdmin` tinyint(4) NOT NULL DEFAULT 0,
+  `aspirasi_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `feedbacks`
+--
+
+INSERT INTO `feedbacks` (`id`, `feedback`, `created_at`, `isAdmin`, `aspirasi_id`) VALUES
+(1, 'Lama COy\r\n', '2022-11-17', 0, 3),
+(2, 'Ditunggu Segera Pak', '2022-11-17', 1, 2),
+(3, 'Ditunggu Secepatnya Pak', '2022-11-17', 0, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `jenis_kategori` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `jenis_kategori`) VALUES
+(1, 'Lingkungan'),
+(2, 'Keamanan'),
+(3, 'Masyarakat');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penduduk`
+--
+
+CREATE TABLE `penduduk` (
+  `id_penduduk` int(11) NOT NULL,
+  `nama_penduduk` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `penduduk`
+--
+
+INSERT INTO `penduduk` (`id_penduduk`, `nama_penduduk`, `alamat`) VALUES
+(1, 'Seto Romie Dharmansyah', 'Jl.Bangun Nusa Raya'),
+(2, 'Kiani Rena', 'Jalan Raya 1'),
+(3, 'Kiani Rena', 'Jalan Raya 1'),
+(4, 'Kiani Rena', 'Jalan Raya 1'),
+(5, 'seto', 'adwad'),
+(6, 'dawdas', 'adwsadwa');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `aspirasi`
+--
+ALTER TABLE `aspirasi`
+  ADD PRIMARY KEY (`id_aspirasi`),
+  ADD KEY `id_penduduk` (`id_penduduk`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indexes for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `penduduk`
+--
+ALTER TABLE `penduduk`
+  ADD PRIMARY KEY (`id_penduduk`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `aspirasi`
+--
+ALTER TABLE `aspirasi`
+  MODIFY `id_aspirasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `penduduk`
+--
+ALTER TABLE `penduduk`
+  MODIFY `id_penduduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `aspirasi`
+--
+ALTER TABLE `aspirasi`
+  ADD CONSTRAINT `aspirasi_ibfk_1` FOREIGN KEY (`id_penduduk`) REFERENCES `penduduk` (`id_penduduk`),
+  ADD CONSTRAINT `aspirasi_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
